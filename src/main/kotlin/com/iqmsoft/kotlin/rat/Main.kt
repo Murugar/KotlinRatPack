ef144e3fc2360cca6abdf140f1db5c1e45ca6cd6
Davanon <davanon2014@gmail.com>
2017-09-25 16:29:56 +0530

    First

---
+++ src/main/kotlin/com/iqmsoft/kotlin/rat/Main.kt
@@ -0,0 +1,20 @@
+package com.iqmsoft.kotlin.rat
+
+import ratpack.server.RatpackServer
+import javax.script.ScriptContext
+import javax.script.ScriptEngineManager
+
+private val engine = ScriptEngineManager().getEngineByName("nashorn")
+private val bindings = engine.createBindings()
+val stream = Class.forName("com.iqmsoft.kotlin.rat.MainKt").getResourceAsStream("/ratpack.js")
+
+fun main(args: Array<String>) {
+    RatpackServer.start { spec ->
+        bindings.put("ratpack", spec)
+        engine.setBindings(bindings, ScriptContext.GLOBAL_SCOPE)
+        val bytes = ByteArray(stream.available())
+        stream.read(bytes)
+        val str = java.lang.String(bytes) as String
+        engine.eval(str)
+    }
+}
